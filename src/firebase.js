00ca5063d8ff72baeeea88c473196dc5c8da7957
유민지 <yyoumi4854@gmail.com>
2023-05-01 17:57:37 +0900

    firebase@9.6.1 세팅

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+// import firebase from "firebase/app";
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDoNgi7Lm7zYGfTvqbGrr_UkNy8n_rPaLM",
+  authDomain: "nwitter-49532.firebaseapp.com",
+  projectId: "nwitter-49532",
+  storageBucket: "nwitter-49532.appspot.com",
+  messagingSenderId: "417485889489",
+  appId: "1:417485889489:web:aa27b6b793d2325b270af0",
+};
+
+export default firebase.initializeApp(firebaseConfig);
