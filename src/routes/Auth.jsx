a26b7a4c102e33366a8900384ce92e36491603f7
유민지 <yyoumi4854@gmail.com>
2023-05-18 19:03:28 +0900

    파이어베이스 구글, 깃허브 로그인

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 
 import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signInWithPopup,
 } from "firebase/auth";
 import { authService } from "fbase";
 
@@ -46,6 +49,21 @@ const Auth = () => {
 
   // newAccount의 이전 값을 가져와서 그 값에 반대되는 것을 리턴
   const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+
+    if (name === "google") {
+      provider = new GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new GithubAuthProvider();
+    }
+
+    const data = await signInWithPopup(authService, provider);
+    console.log(data);
+  };
 
   return (
     <div>
@@ -73,8 +91,12 @@ const Auth = () => {
         {newAccount ? "Sign In" : "Create Account"}
       </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
       </div>
     </div>
   );
