02a6ca5e175745405b14be53a8e3ee821843329f
유민지 <yyoumi4854@gmail.com>
2023-05-19 16:52:00 +0900

    파이어베이스 deleteDos로 delete, updateDoc로 put구현

--- src/components/Nweet.jsx
+++ src/components/Nweet.jsx
@@ -1,14 +1,69 @@
-import React from "react";
+import { dbService } from "fbase";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import React, { useState } from "react";
 
 const Nweet = ({ nweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.text);
+
+  // 리터럴
+  const NweetTextRef = doc(dbService, "nweets", `${nweetObj.id}`);
+
+  const onDeleteClick = async () => {
+    const ok = window.confirm("정말 이 nweet을 삭제하시겠습니까?");
+    console.log(ok);
+    if (ok) {
+      // delete 부분
+      await deleteDoc(NweetTextRef);
+    }
+  };
+
+  const toggleEditing = () => setEditing((prev) => !prev);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    // update 부분
+    await updateDoc(NweetTextRef, {
+      text: newNweet,
+    });
+    setEditing(false);
+  };
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewNweet(value);
+  };
+
   return (
     <div>
-      <h4>{nweetObj.text}</h4>
-      {isOwner && (
-        // 현재 회원이 쓴 트윗인지 확인 가능 -> 누가 주인인지 알 수 있음
+      {editing ? (
         <>
-          <button>Delete Nweet</button>
-          <button>Edit Nweet</button>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Edit your nweet"
+              defaultValue={nweetObj.text}
+              value={newNweet}
+              required
+              onChange={onChange}
+            />
+            <input type="submit" value="Update Nweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{nweetObj.text}</h4>
+          {isOwner && (
+            // 현재 회원이 쓴 트윗인지 확인 가능 -> 누가 주인인지 알 수 있음
+            <>
+              <button onClick={onDeleteClick}>Delete Nweet</button>
+              <button onClick={toggleEditing}>Edit Nweet</button>
+            </>
+          )}
         </>
       )}
     </div>
